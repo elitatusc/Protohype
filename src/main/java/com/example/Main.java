4427fd704bd360f92739da5efe1bf0a73bbedfec
Jessica Greene <jcg300@icloud.com>
2024-11-20 18:48:22 +0000

    editing string

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sup world!");
+        System.out.println("Sup losers!");
     }
 }
