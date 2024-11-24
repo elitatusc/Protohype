31ae2ce182d6094297ce12c0c11302d054e7ae37
Jessica Greene <jcg300@icloud.com>
2024-11-24 17:45:00 +0000

    adding classes

---
+++ src/main/java/com/example/Credentials.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Credentials {
+
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "password";
+    public static final String URL = "jdbc:postgresql://localhost:5432/Recipie_suggestions";
+    //Client-server connection
+    public static final String HOST = "127.0.0.1"; //localhost
+    public static final int PORT = 9994; //This is NOT the port in postgres, but the port at which the RecordsDatabaseServer is listening
+
+}
