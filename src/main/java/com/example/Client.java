cdabb8f66f85afc60b67282d90d73beb425ca973
Jessica Greene <jcg300@icloud.com>
2024-12-05 21:47:19 +0000

    changes to the client class

--- src/main/java/com/example/Client.java
+++ src/main/java/com/example/Client.java
@@ -378,6 +378,7 @@ public class Client extends Application{
             }
         });
 
+        //need to edit these so they fit correctly and fill whole page
         ObservableList<TableColumn<RecipeTable,?>> tmp = recipeTable.getColumns();
         tmp.addAll(recipe_name, prep_time, cooking_time, total_time, difficulty);
 
