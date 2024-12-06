e5bb9c4007883fd5242ea2e978554c8489981749
Elita Tuscano <elita.tuscano@gmail.com>
2024-12-06 22:00:59 +0000

    changed the attendInstructions

--- src/main/java/com/example/Service.java
+++ src/main/java/com/example/Service.java
@@ -113,29 +113,39 @@ public class Service extends Thread {
         this.outcome = null;
         String jdbcURL = "jdbc:h2:mem:receipe";
 
-        String sql = "SELECT\n" +
-                "  r.recipe_name,\n" +
-                "  r.prep_time,\n" +
-                "  r.cook_time,\n" +
-                "  r.difficulty_level\n" +
-                "FROM recipes r\n" +
-                "JOIN recipe_ingredients ri ON r.recipe_id = ri.recipe_id\n" +
-                "LEFT JOIN fridge_items fi ON ri.ingredient_id = fi.ingredient_id\n" +
-                "LEFT JOIN ingredients i ON ri.ingredient_id = i.ingredient_id\n" +
-                "GROUP BY r.recipe_id, r.recipe_name, r.prep_time, r.cook_time, r.difficulty_level\n" +
-                "ORDER BY (SUM(CASE\n" +
-                "    WHEN fi.quantity_available >= ri.quantity_needed THEN 1\n" +
-                "    WHEN ri.quantity_needed IS NULL THEN 0\n" +
-                "    ELSE 0\n" +
-                "  END) * 100.0 / COUNT(ri.ingredient_id))  DESC, r.recipe_name ASC\n" +
-                "LIMIT 5;\n";
+        String sql1 = "SELECT r.recipe_name,r.prep_time,r.cook_time,r.difficulty_level, r.instructions\n" +
+                "FROM recipes r WHERE recipe_name = ?; \n";
+
+        String sql2 = "SELECT i.ingredient_name, ri.quantity_needed, ri.quantity_unit\n" +
+                "FROM recipe_ingredients ri \n" +
+                "LEFT JOIN recipes r ON ri.recipe_id = r.recipe_id\n" +
+                "LEFT JOIN ingredients i ON i.ingredient_id = ri.ingredient_id\n" +
+                "WHERE r.recipe_id = (\n" +
+                "    SELECT recipe_id \n" +
+                "    FROM recipes\n" +
+                "    WHERE recipe_name = ?\n" +
+                ");\n";
 
         Connection connection = DriverManager.getConnection(jdbcURL);
-        PreparedStatement pstmt = connection.prepareStatement(sql);
+        PreparedStatement pstmt = connection.prepareStatement(sql1);
+        PreparedStatement prepSts = connection.prepareStatement(sql2);
+        pstmt.setString(1, this.requestStr[0]);
+        prepSts.setString(1, this.requestStr[0]);
         ResultSet rs = pstmt.executeQuery();
+        ResultSet rs1 = prepSts.executeQuery();
         RowSetFactory aFactory = RowSetProvider.newFactory();
         CachedRowSet crs = aFactory.createCachedRowSet();
         crs.populate(rs);  //need to reset the iterator of rs??
+        while (rs.next()){
+            crs.moveToInsertRow(); // moves to the next row to insert in
+            ResultSetMetaData currentRsMetaData = rs1.getMetaData(); // gets data about the rs1 resultset, allowing us to get the column count
+            int numberOfColumns = currentRsMetaData.getColumnCount();// gets the column count
+            for (int i = 0; i < numberOfColumns; i++){
+                crs.updateObject(i, rs1.getObject(i)); //adds it
+            }
+            crs.insertRow(); //inserts it
+            crs.moveToCurrentRow();
+        }
         crs.beforeFirst();
         this.outcome = crs; //now populated
 
@@ -188,8 +198,11 @@ public class Service extends Thread {
                 String cook_time = outcome.getString("cook_time");
                 String difficulty_level = outcome.getString("difficulty_level");
                 String instructions = outcome.getString("instructions");
+                String ingredient_name = outcome.getString("ingredient_name");
+                String quantity_needed = outcome.getString("quantity_needed");
+                String quantity_unit = outcome.getString("quantity_unit");
 
-                System.out.println(recipe_name + " " + prep_time + " " + cook_time + " " + difficulty_level + " " + instructions);
+                System.out.println(recipe_name + " " + prep_time + " " + cook_time + " " + difficulty_level + " " + instructions + " " + ingredient_name + " " + quantity_needed + " " + quantity_unit);
             }
             this.outcome.beforeFirst();
             OutputStream outcomeStream = this.serviceSocket.getOutputStream();
