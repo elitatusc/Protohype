3b2d7e544c77987d60bc0ffdd7929de44c989761
Millie <axw348@student.bham.ac.uk>
2024-12-11 03:18:28 +0000

    Add test for H2 database
    - Also add 2 tests for attendInstructions and attendIngredients

--- src/test/java/com/example/ServiceTest.java
+++ src/test/java/com/example/ServiceTest.java
@@ -9,9 +9,14 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.sql.rowset.CachedRowSet;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -32,5 +37,40 @@ public class ServiceTest {
         Boolean aBoolean = service.attendRequest();
         assertEquals(aBoolean, Boolean.TRUE);
 
+    }
+
+//    @Test
+//    public void testAttendInstructions() throws Exception {
+//        ResultSet resultSet = service.attendInstructions();
+//        int linesCount = 0;
+//        while (resultSet.next()){
+//            linesCount++;
+//        }
+//        assertNotNull(resultSet, "The instructions fetched is NULL");
+//        assertTrue(linesCount > 0, "No data has been retrieved");
+//    }
+
+//    @Test
+//    public void testAttendIngredients() throws Exception {
+//        ResultSet resultSet = service.attendIngredients();
+//        int linesCount = 0;
+//        while (resultSet.next()){
+//            linesCount++;
+//        }
+//        assertNotNull(resultSet, "The ingredients fetched is NULL");
+//        assertTrue(linesCount > 0, "No data has been retrieved");
+//    }
+
+    @Test
+    public void testH2Connection() throws Exception {
+        String jdbcURL = "jdbc:h2:mem:receipe";
+        Connection connection = DriverManager.getConnection(jdbcURL);
+        Statement statement = connection.createStatement();
+        String myQuery3 = "SELECT * FROM recipes WHERE recipe_id = 0;";
+        ResultSet resultSet = statement.executeQuery(myQuery3);
+        //To test our connection, we query the database and ensure it returns the right thing
+
+        assertTrue(resultSet.next(), "The table has no rows");
+        assertEquals("Eggs On Toast", resultSet.getString("recipe_name"), "The recipe name returned is wrong");
     }
 }
